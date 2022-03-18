13230961777f4126d6ce8a67ee9f5eff430e45a4
lindenjo <79539710+lindenjo@users.noreply.github.com>
2022-03-18 13:18:34 +0800

    Initial commit

---
+++ javascript/index.js
@@ -0,0 +1,27 @@
+// String argument, that will decide what note to play.
+function makeSound(note) {
+  let noteAudio = new Audio("sounds/" + note + ".mp3");
+  noteAudio.play();
+}
+
+
+let buttons = document.querySelectorAll('button');
+for (let i = 0; i < buttons.length; i++) {
+  let note =  buttons[i].classList[0];
+  buttons[i].addEventListener("click", function() {
+    makeSound(note)
+  })
+}
+
+// Creating object to bind keys to note
+let keyBindElements = document.querySelectorAll('button p.key-bind')
+let keyBindings = {}
+for(let i = 0; i < keyBindElements.length; i++){
+  keyBindings[keyBindElements[i].innerText.toLowerCase()] = buttons[i].classList[0];
+}
+// If the key pressed is a key-binding, it will makeSound()
+document.addEventListener("keydown", function(event) {
+  if(event.key in keyBindings) {
+    makeSound(keyBindings[event.key])
+  }
+})
